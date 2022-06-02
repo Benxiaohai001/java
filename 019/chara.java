73afef42d59f745d5446890d84d8e47eddcf5f45
benxiaohai <18434391952@163.com>
2022-06-02 18:36:22 +0000

    char

---
+++ 019/chara.java
@@ -0,0 +1,9 @@
+public class chara{
+	public static void main(String[] args){
+		char c = 65;		//字符型和整形可以互换
+		//char d = c + 1;
+		System.out.println(c +1);
+		int d = '薛';		//34203
+		System.out.println(d);
+	}
+}
